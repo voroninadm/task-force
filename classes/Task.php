6b34d91a23136532f4cf4deac753197e97fa52c1
Alex <voronin.adm@mail.ru>
2022-06-21 14:59:31 +0300

    task 1. Test class added

---
+++ classes/Task.php
@@ -0,0 +1,15 @@
+<?php
+
+class Task
+{
+    const STATUS_NEW = 'new';
+    const STATUS_CANCELED = 'canceled';
+    const STATUS_IN_WORK = 'in_work';
+    const STATUS_DONE = 'done';
+    const STATUS_FAILED = 'failed';
+
+    const ACTION_DECLINE = 'decline';
+    const ACTION_RESPOND = 'respond';
+    const ACTION_DONE = 'done';
+    const ACTION_REFUSE = 'refuse';
+}
