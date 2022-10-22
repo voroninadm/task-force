9309e68b36204d79cdf2f1b8d1659e8e60fb75bc
Alex <voronin.adm@mail.ru>
2022-10-22 19:25:50 +0300

    fixtures fixed

--- fixtures/templates/response.php
+++ fixtures/templates/response.php
@@ -7,7 +7,6 @@ $users_count = 15;
 
 return [
     'task_id' => $faker->numberBetween(0, $tasks),
-    'task_budget' => $faker->numberBetween(500, 10000),
     'user_id' => $faker->numberBetween(0, $tasks),
     'comment' => $faker->paragraph(),
     'create_date' => $faker->dateTimeBetween('-2 days', 'now')->format('Y-m-d H:i:s'),
